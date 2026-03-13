df130e5ebaebcafd509ee73b6f4170e886389982
trikki12 <144922665+trikki12@users.noreply.github.com>
2026-03-13 19:22:13 +0000

    Add files via upload

---
+++ Laba1/Core/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Core
+{
+    public class Note
+    {
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+
+    public class Category
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public bool IsImportant { get; set; }
+    }
+
+    public class User
+    {
+        public string Username { get; set; }
+    }
+}
