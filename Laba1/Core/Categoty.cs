d1373f4572560c0bc43c12b03b761d787f2243f1
trikki12 <144922665+trikki12@users.noreply.github.com>
2026-03-16 17:08:30 +0000

    Add files via upload

---
+++ Laba1/Core/Categoty.cs
@@ -0,0 +1,10 @@
+﻿namespace Core
+{
+    public class Category
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public bool IsImportant { get; set; }
+    }
+}
+
